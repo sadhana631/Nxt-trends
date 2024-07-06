e8d44f7bbb8952c938a91ee53e6054c693160c9c
sadhana631 <indhusweety631@gnail.com>
2024-07-06 08:17:49 +0000

    first commit

---
+++ src/components/FiltersGroup/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const FiltersGroup = () => (
+  <div className="filters-group-container">
+    <h1>Filters Group</h1>
+    const renderRatingFiltersList = () => {
+  </div>
+)
+
+export default FiltersGroup
